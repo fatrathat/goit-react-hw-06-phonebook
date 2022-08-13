b068b744f0653067802537e6335fc2e34b429d54
Eugene <eugene.multan@gmail.com>
2022-08-13 13:00:53 +0300

    add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
 import { contacts } from './reducers';
+
+import { persistStore } from 'redux-persist';
 
 const store = configureStore({
   reducer: {
     contacts,
   },
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['persist/PERSIST'],
+    },
+  }),
 });
+
+export let persistor = persistStore(store);
 
 export default store;
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,17 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { items } from './contacts-reducers';
 import { filter } from './filters-redusers';
 
-export const contacts = combineReducers({
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const rootReduser = combineReducers({
   items,
   filter,
 });
+
+export const contacts = persistReducer(persistConfig, rootReduser);
